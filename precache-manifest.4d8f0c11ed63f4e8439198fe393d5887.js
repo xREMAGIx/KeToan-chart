b544bb7d05d033dcb4a58fba6d2aa76654976560
xREMAGIx0 <tramdo2806@gmail.com>
2020-06-11 14:19:30 +0700

    Updates

---
+++ precache-manifest.4d8f0c11ed63f4e8439198fe393d5887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae36248ddba7cd8bf37bbb6c63136a18",
+    "url": "/KeToan-chart/index.html"
+  },
+  {
+    "revision": "9aa1ae3bfb7996dfa871",
+    "url": "/KeToan-chart/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bb096e6576acff040a47",
+    "url": "/KeToan-chart/static/js/2.e5a1986f.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/KeToan-chart/static/js/2.e5a1986f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa1ae3bfb7996dfa871",
+    "url": "/KeToan-chart/static/js/main.3d107ec7.chunk.js"
+  },
+  {
+    "revision": "ad72aec7df1f87f6b44d",
+    "url": "/KeToan-chart/static/js/runtime-main.026b98de.js"
+  }
+]);
